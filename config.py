4e5425bea835cec0ca5fe44a0a4c9b43baa8daec
CoolinPack <zalyalievarsen7@gmail.com>
2026-08-19 06:22:10 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
 
 # Добавьте ID вашей группы
-GROUP_CHAT_ID = int(os.getenv("GROUP_CHAT_ID", -1004208823431))
+GROUP_CHAT_ID = int(os.getenv("GROUP_CHAT_ID", -5406433901))
 
 DB_PATH = os.path.join(os.path.dirname(__file__), "terassa.db")
 MENU_JSON_PATH = os.path.join(os.path.dirname(__file__), "public", "menu.json")
