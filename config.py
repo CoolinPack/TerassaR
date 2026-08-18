3b0fe225d58b4354002c382b4a3b5a1ba12afc12
CoolinPack <zalyalievarsen7@gmail.com>
2026-08-19 05:43:15 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
-# Токен вашего Telegram-бота (лучше заменить на переменные окружения в продакшене)
 BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
 
-# Пути к файлам и базе данных
+# Добавьте ID вашей группы
+GROUP_CHAT_ID = int(os.getenv("GROUP_CHAT_ID", -1004208823431))
+
 DB_PATH = os.path.join(os.path.dirname(__file__), "terassa.db")
 MENU_JSON_PATH = os.path.join(os.path.dirname(__file__), "public", "menu.json")
